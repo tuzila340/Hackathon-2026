129a71d7c6b0492aa28cfa0dcf7a52a4cd576dbc
tuzila <221207985+tuzila340@users.noreply.github.com>
2026-02-01 10:17:20 +0100

    created and add leaf functio for main html and game html

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const imgs = [
+    '../img/tree_leaf_1.png',
+    '../img/tree_leaf_2.png',
+    '../img/tree_leaf_3.png',
+    '../img/tree_leaf_4.png',
+];
+
+const makeBubble = () => {
+    const b = document.createElement('div');
+    b.className = 'bubble';
+    b.style.left = Math.random() * 100 + 'vw';
+    b.style.animationDuration = (6 + Math.random() * 8) + 's';
+    b.style.width = b.style.height = (40 + Math.random() * 60) + 'px';
+    document.body.appendChild(b);
+    b.style.backgroundImage = `url(${imgs[Math.floor(Math.random() * imgs.length)]})`;
+    setTimeout(() => b.remove(), 15000);
+  };
+  setInterval(makeBubble, 500);
