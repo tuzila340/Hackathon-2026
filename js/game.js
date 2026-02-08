da321f9e590f191b88287515b73adec5681343b1
tuzila <221207985+tuzila340@users.noreply.github.com>
2026-02-08 23:21:39 +0100

    drag and drop logic for game

--- js/game.js
+++ js/game.js
@@ -7,9 +7,9 @@ const imgs = [
     '../img/tree_leaf_4.png',
 ];
 
-const makeBubble = () => {
+const makeGarbage = () => {
     const b = document.createElement('div');
-    b.className = 'leaf';
+    b.className = 'garbage';
     b.style.left = Math.random() * 100 + 'vw';
     b.style.top = Math.random() * 100 + 'vh';
     b.style.width = b.style.height = (40 + Math.random() * 60) + 'px';
@@ -17,5 +17,40 @@ const makeBubble = () => {
     b.style.backgroundImage = `url(${imgs[Math.floor(Math.random() * imgs.length)]})`;
   };
   for (let i = 0; i < 10; i++) {
-    makeBubble();
+    makeGarbage();
 }
+
+let elements = document.querySelectorAll('.garbage');
+
+elements.forEach(element => {
+  element.onmousedown = function(event) {
+    let shiftX = event.clientX - element.getBoundingClientRect().left;
+    let shiftY = event.clientY - element.getBoundingClientRect().top;
+
+    element.style.position = 'absolute';
+    element.style.zIndex = 1000;
+    document.body.append(element);
+
+    moveAt(event.pageX, event.pageY);
+
+    function moveAt(pageX, pageY) {
+      element.style.left = pageX - shiftX + 'px';
+      element.style.top = pageY - shiftY + 'px';
+    }
+
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    element.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      element.onmouseup = null;
+    };
+  };
+
+  element.ondragstart = function() {
+    return false;
+  };
+});
